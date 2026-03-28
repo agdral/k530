eb031bfaabf355258328edede72c5f1a4d029cec
agdral <ezequiel.escobar@proton.me>
2026-03-28 02:08:47 -0300

    new layouts, new structure of variables, simplificate code

--- keymaps/default/keymap.c
+++ keymaps/default/keymap.c
@@ -1,36 +1,40 @@
 #include "keymap.h"
-#include "lateral.h"
 #include "layout.h"
-#include "variables.h"
+#include "../variables.h"
 
 enum custom_keycodes {
-    // Password
-    P_1 = SAFE_RANGE,
-    P_2, P_3, P_4, P_5, P_6, P_7, P_8,
-    // Mails
-    Mail_1, Mail_2, Mail_3, Mail_4, Mail_5,
-    // Password
-    Pass_1, Pass_2, Pass_3, Pass_4, Pass_5,
-    // Ip
-    Ip_1, Ip_2, Ip_3,
+    TEST = SAFE_RANGE,
+
+    #define X(name, normal, meta) name,
+    CUSTOM_TOP
+    #undef X
+
+    #define X(name, normal) name,
+    CUSTOM_VAR
+    #undef X
 };
+
+static const custom_top_t custom_top[] = {
+    #define X(name, normal, meta) { name, normal, meta },
+    CUSTOM_TOP
+    #undef X
+};
+
+static const custom_var_t custom_var[] = {
+    #define X(name, normal) { name, normal },
+    CUSTOM_VAR
+    #undef X
+};
+
 
 enum layer_names {
     // layout
-    _BASE, _SUPA, _PASS, _QWER,
+    _BASE, _PASS, _QWER,
     // lateral left
     _LTOP, _LMID, _LBOT,
     // lateral right
     _RTOP, _RMID, _RBOT,
 };
-
-static bool send_oracion_enter(keyrecord_t *record, const char *my_str) {
-    if (record->event.pressed) {
-        send_string(my_str);
-        SEND_STRING(SS_TAP(X_ENT));
-    }
-    return false;
-}
 
 static bool send_oracion(keyrecord_t *record, const char *my_str) {
     if (record->event.pressed) {
@@ -39,61 +43,40 @@ static bool send_oracion(keyrecord_t *record, const char *my_str) {
     return false;
 }
 
-static bool layer_mod_kc(keyrecord_t *record, int modifs) {
-    if (record->tap.count == 0) {
-        if (record->event.pressed) {
-            register_mods(modifs);
-        } else {
-            unregister_mods(modifs);
+bool process_record_user(uint16_t keycode, keyrecord_t *record) {
+    for (uint8_t i = 0; i < CUSTOM_VAR_COUNT; i++) {
+        if (keycode == custom_var[i].keycode) {
+            return send_oracion(record, custom_var[i].normal_key);
         }
     }
-    return true;
-}
 
-bool process_record_user(uint16_t keycode, keyrecord_t *record) {
-    switch (keycode) {
-        case P_1: return send_oracion_enter(record, V_P_1);
-        case P_2: return send_oracion_enter(record, V_P_2);
-        case P_3: return send_oracion_enter(record, V_P_3);
-        case P_4: return send_oracion_enter(record, V_P_4);
-        case P_5: return send_oracion_enter(record, V_P_5);
-        case P_6: return send_oracion_enter(record, V_P_6);
-        case P_7: return send_oracion_enter(record, V_P_6);
-        case P_8: return send_oracion_enter(record, V_P_6);
-        case Mail_1: return send_oracion(record, V_Mail_1);
-        case Mail_2: return send_oracion(record, V_Mail_2);
-        case Mail_3: return send_oracion(record, V_Mail_3);
-        case Mail_4: return send_oracion(record, V_Mail_4);
-        case Mail_5: return send_oracion(record, V_Mail_5);
-        case Pass_1: return send_oracion(record, V_Mail_C1);
-        case Pass_2: return send_oracion(record, V_Mail_C2);
-        case Pass_3: return send_oracion(record, V_Mail_C3);
-        case Pass_4: return send_oracion(record, V_Mail_C4);
-        case Pass_5: return send_oracion(record, V_Mail_C5);
-        case Ip_1: return send_oracion(record, V_IP_1);
-        case Ip_2: return send_oracion(record, V_IP_2);
-        case Ip_3: return send_oracion(record, V_IP_3);
-        case HY_LTOP: return layer_mod_kc(record, MOD_LALT | MOD_LCTL | MOD_LGUI);
-        case HY_LMID: return layer_mod_kc(record, MOD_LGUI | MOD_LCTL);
-        case HY_CENT: return layer_mod_kc(record, MOD_LGUI);
-        case HY_RMID: return layer_mod_kc(record, MOD_LGUI | MOD_LALT);
-        case HY_RTOP: return layer_mod_kc(record, MOD_LALT);
+    for (uint8_t i = 0; i < CUSTOM_TOP_COUNT; i++) {
+        if (keycode == custom_top[i].keycode) {
+            if (!record->event.pressed) return false;
+            uint8_t mods = get_mods();
+            if (mods) { 
+                tap_code16(custom_top[i].mod_key);
+            } else {
+                tap_code16(custom_top[i].normal_key);
+            }
+            return false;
+        }
     }
+
     return true;
 }
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     // layout
     [_BASE] = MY_LAYOUT(BASE_LAYER), 
-    [_SUPA] = MY_LAYOUT(SUPA_LAYER), 
     [_PASS] = MY_LAYOUT(PASS_LAYER),
     [_QWER] = MY_LAYOUT(QWERTY_LAYER),
     // lateral left
-    [_LTOP] = MY_LAYOUT(NUM_LAYER), 
+    [_LTOP] = MY_LAYOUT(NUMFN_LAYER), 
     [_LMID] = MY_LAYOUT(ARROW_LAYER),
     [_LBOT] = MY_LAYOUT(MOUSE_LAYER),
     // lateral right
-    [_RTOP] = MY_LAYOUT(FN_NORMAL), 
+    [_RTOP] = MY_LAYOUT(NUMFN_LAYER), 
     [_RMID] = MY_LAYOUT(SUB_LAYER), 
-    [_RBOT] = MY_LAYOUT(FN_EXTENT)
+    [_RBOT] = MY_LAYOUT(MOUSE_LAYER)
 };
--- keymaps/default/layout.h
+++ keymaps/default/layout.h
@@ -1,32 +1,61 @@
 #ifndef LAYOUT_H
 #define LAYOUT_H
 
+// Layout
 #define BASE_LAYER \
-    CT_X   ,  CT_C   ,  CT_V   ,  CT_Z   ,  CT_Y   ,  CT_S   ,  KC_MPRV,  KC_MNXT,  KC_VOLD,  KC_VOLU,  KC_MPLY,  KC_MUTE,  KC_MSTP,  TO_RSOP, \
+    TOP_A  ,  TOP_B  ,  TOP_C  ,  TOP_D  ,  TOP_E  ,  TOP_F  ,  TOP_G  ,  TOP_H  ,  TOP_I  ,  TOP_J  ,  TOP_K  ,  TOP_M  ,  TOP_N  ,  TO_RSOP, \
     TO_LTOP,  KC_QUOT,  KC_COMM,  KC_DOT ,  KC_P   ,  KC_Y   ,  KC_CAPS,  KC_F   ,  KC_G   ,  KC_C   ,  KC_H   ,  KC_L   ,  KC_SLSH,  TO_RTOP, \
     TO_LMID,  KC_A   ,  KC_O   ,  KC_E   ,  KC_U   ,  KC_I   ,  XP_MTOP,  KC_D   ,  KC_R   ,  KC_T   ,  KC_N   ,  KC_S   ,            TO_RMID, \
-    TO_LBOT,            KC_Q   ,  KC_J   ,  KC_X   ,  KC_K   ,  KC_MINS,  KC_Z   ,  KC_B   ,  KC_M   ,  KC_W   ,  KC_V   ,            TO_RBOT, \
+    TO_LBOT,  KC_Q   ,  KC_J   ,  KC_X   ,  KC_K   ,  KC_MINS,            KC_Z   ,  KC_B   ,  KC_M   ,  KC_W   ,  KC_V   ,            TO_RBOT, \
     HY_LTOP,  HY_LMID,  HY_LBOT,                                HY_CENT,                                HY_RBOT,  HY_RMID,  HY_RTOP,  QWERTYS  \
-
-#define SUPA_LAYER \
-    KC_GRV ,  KC_1,     KC_2,     KC_3,     KC_4,     KC_5,     KC_6,     KC_7,     KC_8,     KC_9,     KC_0,     KC_BSLS,  KC_EQL,   _______, \
-    _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______, \
-    _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,            _______, \
-    _______,            _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,            _______, \
-    _______,  _______,  _______,                                _______,                                _______,  _______,  _______,  _______  \
 
 #define QWERTY_LAYER \
     KC_GRV ,  KC_1   ,  KC_2   ,  KC_3   ,  KC_4   ,  KC_5   ,  KC_6  ,   KC_7   ,  KC_8   ,  KC_9   ,  KC_0   ,  KC_SLSH,  KC_EQL ,  _______, \
     KC_TAB ,  KC_Q   ,  KC_W   ,  KC_E   ,  KC_R   ,  KC_T   ,  KC_Y  ,   KC_U   ,  KC_I   ,  KC_O   ,  KC_P   ,  KC_LBRC,  KC_RBRC,  _______, \
-    KC_ESC ,  KC_A   ,  KC_S   ,  KC_D   ,  KC_F   ,  KC_G   ,  KC_H  ,   KC_J   ,  KC_K   ,  KC_L   ,  KC_SCLN,  KC_QUOT,            _______, \
-    KC_LSFT,            KC_Z   ,  KC_X   ,  KC_C   ,  KC_V   ,  KC_B  ,   KC_N   ,  KC_M   ,  KC_COMM,  KC_DOT ,  KC_SLSH,            KC_BSPC, \
+    KC_ESC ,  KC_A   ,  KC_S   ,  KC_D   ,  KC_F   ,  KC_G   ,  KC_H   ,  KC_J   ,  KC_K   ,  KC_L   ,  KC_SCLN,  KC_QUOT,            _______, \
+    KC_LSFT,  KC_Z   ,  KC_X   ,  KC_C   ,  KC_V   ,  KC_B   ,            KC_N   ,  KC_M   ,  KC_COMM,  KC_DOT ,  KC_SLSH,            KC_BSPC, \
     KC_LCTL,  KC_LGUI,  KC_LALT,                                KC_SPC,                                 KC_LCTL,  KC_RALT,  KC_RGUI,  QWERTYS  \
 
 #define PASS_LAYER \
-    _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______, \
-    _______,  P_1    ,  P_2    ,  P_3    ,  P_4    ,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______, \
-    _______,  P_5    ,  P_6    ,  P_7    ,  P_8    ,  _______,  _______,  Mail_1 ,  Mail_2 ,  Mail_3 ,  Mail_4 ,  Mail_5 ,            _______, \
-    _______,            Ip_1   ,  Ip_2   ,  Ip_3   ,  _______,  _______,  Pass_1 ,  Pass_2 ,  Pass_3 ,  Pass_4 ,  Pass_5 ,            _______, \
+    _______,  PASS_A ,  PASS_B ,  PASS_C ,  PASS_D ,  PASS_E ,  _______,  MAIL_A ,  MAIL_B ,  MAIL_C ,  MAIL_D ,  MAIL_E ,  _______,  _______, \
+    _______,  PASS_F ,  PASS_G ,  PASS_H ,  PASS_I ,  PASS_J ,  _______,  MASS_A ,  MASS_B ,  MASS_C ,  MASS_D ,  MASS_E ,  _______,  _______, \
+    _______,  PASS_K ,  PASS_M ,  PASS_N ,  PASS_O ,  PASS_P ,  _______,  MAIL_F ,  MAIL_G ,  MAIL_H ,  MAIL_I ,  MAIL_J ,            _______, \
+    _______,  PASS_Q ,  PASS_R ,  PASS_S ,  PASS_T ,  PASS_U ,            MASS_F ,  MASS_G ,  MASS_H ,  MASS_I ,  MASS_J ,            _______, \
     _______,  _______,  _______,                                _______,                                _______,  _______,  _______,  _______  \
+
+
+// Both Layout
+#define MOUSE_LAYER \
+    _______,  _______,  MS_WHLL,  MS_WHLR,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  QK_BOOT, \
+    _______,  MS_WHLU,  MS_BTN1,  MS_UP  ,  MS_BTN2,  CT_PGUP,  _______,  _______,  SH_SCLN,  SH_9   ,  SH_0   ,  SH_SCLN,  _______,  _______, \
+    _______,  MS_WHLD,  MS_LEFT,  MS_DOWN,  MS_RGHT,  CT_PGDN,  _______,  KC_EQL ,  SH_LBRC,  KC_LBRC,  KC_RBRC,  SH_RBRC,            _______, \
+    _______,  _______,  A_LEFT ,  A_RIGHT,  MS_BTN3,  _______,            _______,  KC_TILD,  KC_SCLN,  KC_GRV,   KC_QUES,            _______, \
+    _______,  _______,  _______,                                _______,                                _______,  _______,  _______,  _______  \
+
+#define NUMFN_LAYER \
+    _______,  KC_1   ,  KC_2   ,  KC_3   ,  KC_PDOT,  _______,  _______,  _______,  _______,  KC_F1  ,  KC_F2  ,  KC_F3  ,  KC_F4  ,  KC_F12 , \
+    _______,  KC_4   ,  KC_5   ,  KC_6   ,  KC_0   ,  _______,  _______,  _______,  _______,  KC_F5  ,  KC_F6  ,  KC_F7  ,  KC_F8  ,  _______, \
+    _______,  KC_7   ,  KC_8   ,  KC_9   ,  KC_EQL ,  _______,  _______,  _______,  _______,  KC_F9  ,  KC_F10 ,  KC_F11 ,            _______, \
+    _______,  _______,  _______,  _______,  _______,  _______,            _______,  _______,  _______,  _______,  _______,            _______, \
+    _______,  _______,  _______,                                _______,                                _______,  _______,  _______,  _______  \
+
+
+// Left Layout
+#define ARROW_LAYER \
+    _______,  _______,  MS_WHLL,  MS_WHLR,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  QK_BOOT, \
+    _______,  MS_WHLU,  KC_HOME,  KC_UP  ,  KC_END,   CT_PGUP,  _______,  KC_MINS,  KC_1   ,  KC_2   ,  KC_3   ,  KC_PDOT,  _______,  _______, \
+    _______,  MS_WHLD,  KC_LEFT,  KC_DOWN,  KC_RGHT,  CT_PGDN,  _______,  KC_PLUS,  KC_4   ,  KC_5   ,  KC_6   ,  KC_0   ,            _______, \
+    _______,  _______,  A_LEFT ,  A_RIGHT,  MS_BTN3,  _______,            _______,  KC_ASTR,  KC_7   ,  KC_8   ,  KC_9   ,            _______, \
+    _______,  _______,  _______,                                _______,                                _______,  _______,  _______,  _______  \
+
+
+// Rigth Layout
+#define SUB_LAYER \
+    _______,  _______,  RM_PREV,  RM_NEXT,  RM_TOGG,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______, \
+    _______,  RM_SPDU,  RM_SPDD,  RM_VALD,  RM_VALU,  _______,  _______,  _______,  _______,  CT_PGUP,  KC_PSCR,  KC_HOME,  KC_END ,  _______, \
+    _______,  KC_F13 ,  KC_F14 ,  KC_F15 ,  KC_F16 ,  _______,  _______,  _______,  _______,  CT_PGDN,  KC_PGUP,  KC_PGDN,            _______, \
+    _______,  KC_F17 ,  KC_F18 ,  KC_F19 ,  KC_F20 ,  _______,            _______,  _______,  _______,  KC_INS ,  KC_DEL ,            _______, \
+    _______,  _______,  _______,                                _______,                                _______,  _______,  _______,  _______  \
+
 
 #endif
